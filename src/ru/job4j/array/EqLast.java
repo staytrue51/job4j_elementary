b5a57a1381858ccee363a3a2ac9b92a17db12ffd
Viktor <staytrue.sm@yandex.ru>
2021-01-24 13:53:09 +0300

    6.4.1. Сравнить последние элементы двух массивов [#423935]

---
+++ src/ru/job4j/array/EqLast.java
@@ -0,0 +1,10 @@
+package ru.job4j.array;
+
+public class EqLast {
+    public static boolean check(int[] left, int[] right) {
+        if (left[2] == right[2]) {
+        return true;
+        }
+        return false;
+    }
+}

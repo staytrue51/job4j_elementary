c971c7a3912c065d622d77247ff915e71285928f
Viktor <staytrue.sm@yandex.ru>
2021-01-26 18:51:00 +0300

    6.7.4. Выигрышные комбинации в сокобан [#423918]

--- src/ru/job4j/array/MatrixCheck.java
+++ src/ru/job4j/array/MatrixCheck.java
@@ -30,4 +30,15 @@ public class MatrixCheck {
         }
         return rsl;
     }
+
+    public static boolean isWin(char[][] board) {
+        boolean result = false;
+        for (int cell = 0; cell < board.length; cell++) {
+            if (monoHorizontal(board, cell) || monoVertical(board, cell)) {
+                result = true;
+                break;
+            }
+        }
+        return result;
+    }
 }
